37e4f1a2fa92d28068eecf4d1bacda85a88137fc
Lucas Lopes <dev.thuram@gmail.com>
2022-01-21 09:10:25 -0300

    test: ensure all elements is rendered

--- src/components/Search/test.spec.tsx
+++ src/components/Search/test.spec.tsx
@@ -4,5 +4,16 @@ import { render, screen } from '@testing-library/react'
 import { Search } from '.'
 
 describe('<Search />', () => {
-  it('should render correctly', () => {})
+  it('should render correctly', () => {
+    render(<Search />)
+
+    expect(screen.getByRole('form')).toBeInTheDocument()
+    expect(screen.getByRole('textbox')).toHaveAttribute(
+      'placeholder',
+      'Digite seu CEP (apenas números)'
+    )
+    expect(
+      screen.getByRole('button', { name: /buscar cep/i })
+    ).toBeInTheDocument()
+  })
 })
