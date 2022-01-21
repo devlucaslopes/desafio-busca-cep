c7e01b9aae07e4023c048eaac26d3fe8e6184213
Lucas Lopes <dev.thuram@gmail.com>
2022-01-21 09:30:20 -0300

    feat: create cep state and handleSubmit()

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const Search = () => (
-  <form name="search">
-    <input type="text" placeholder="Digite seu CEP (apenas números)" />
-    <button type="submit">Buscar CEP</button>
-  </form>
-)
+type SearchType = {
+  onSubmit: (cep: string) => void
+}
+
+export const Search = ({ onSubmit }: SearchType) => {
+  const [cep, setCep] = useState('')
+
+  const handleSubmit = () => {
+    onSubmit(cep)
+  }
+
+  return (
+    <form name="search" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Digite seu CEP (apenas números)"
+        value={cep}
+        onChange={(ev) => setCep(ev.target.value)}
+      />
+      <button type="submit">Buscar CEP</button>
+    </form>
+  )
+}
