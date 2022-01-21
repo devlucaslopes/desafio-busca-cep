df8e453cb58dc2f961c4b6198b0b4a0e13b42484
Lucas Lopes <dev.thuram@gmail.com>
2022-01-21 08:57:19 -0300

    feat: created search component

---
+++ src/components/Search/test.spec.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import { Search } from '.'
+
+describe('<Search />', () => {
+  it('should render correctly', () => {})
+})
