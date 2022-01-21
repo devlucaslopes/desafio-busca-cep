7771c719a93b207371c467cbd385e822a2d9c089
Lucas Lopes <dev.thuram@gmail.com>
2022-01-21 08:51:56 -0300

    feat: received children as a prop

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -2,4 +2,12 @@ import React from 'react'
 
 import './style.scss'
 
-export const Wrapper = () => <div className="wrapper" data-testid="wrapper" />
+type WrapperType = {
+  children: React.ReactNode
+}
+
+export const Wrapper = ({ children }: WrapperType) => (
+  <div className="wrapper" data-testid="wrapper">
+    {children}
+  </div>
+)
