bb4eabd7cac2897e8dfae98dd412322ce14a039c
Lucas Lopes <dev.thuram@gmail.com>
2022-01-21 09:19:02 -0300

    test: ensure update input value

--- src/components/Search/test.spec.tsx
+++ src/components/Search/test.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { Search } from '.'
 
@@ -15,5 +16,17 @@ describe('<Search />', () => {
     expect(
       screen.getByRole('button', { name: /buscar cep/i })
     ).toBeInTheDocument()
+  })
+
+  it('should update input value when user typed', () => {
+    render(<Search />)
+
+    const input = screen.getByRole('textbox')
+
+    expect(input).toHaveValue('')
+
+    userEvent.type(input, '32110290')
+
+    expect(input).toHaveValue('32110290')
   })
 })
