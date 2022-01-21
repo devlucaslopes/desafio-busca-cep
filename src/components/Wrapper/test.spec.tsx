f9fbb44ee81a8dc0b5cc652e7bbddb1face5ffd8
Lucas Lopes <dev.thuram@gmail.com>
2022-01-21 08:52:05 -0300

    test: ensure wrapper render children prop

--- src/components/Wrapper/test.spec.tsx
+++ src/components/Wrapper/test.spec.tsx
@@ -5,11 +5,25 @@ import { Wrapper } from '.'
 
 describe('<Wrapper />', () => {
   it('should render correctly', () => {
-    render(<Wrapper />)
+    render(
+      <Wrapper>
+        <h1>Lorem Ipsum</h1>
+      </Wrapper>
+    )
 
     const component = screen.getByTestId('wrapper')
 
     expect(component).toBeInTheDocument()
     expect(component).toHaveClass('wrapper')
+  })
+
+  it('should render children component', () => {
+    render(
+      <Wrapper>
+        <h1>Lorem Ipsum</h1>
+      </Wrapper>
+    )
+
+    expect(screen.getByText(/lorem ipsum/i)).toBeInTheDocument()
   })
 })
