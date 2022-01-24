f2d7d8304624607e14be87e4f32204baad8e6281
Lucas Lopes <dev.thuram@gmail.com>
2022-01-24 08:14:07 -0300

    feat: added container style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import { Search } from './components/Search'
+import { Wrapper } from './components/Wrapper'
+
+import './App.scss'
+
 function App() {
   return (
     <main>
-      <h1>Busca cep</h1>
+      <div className="container">
+        <h1>Busca cep</h1>
+
+        <Wrapper>
+          <Search onSubmit={() => console.log('oi')} />
+        </Wrapper>
+      </div>
     </main>
   )
 }
