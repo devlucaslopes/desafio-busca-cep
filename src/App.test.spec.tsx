554156e0b03ad88299d71829fd38ff5ed6edd35f
Lucas Lopes <dev.thuram@gmail.com>
2022-01-25 08:57:43 -0300

    refactor: added axios mock adapter

--- src/App.test.spec.tsx
+++ src/App.test.spec.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 
 import App from './App'
 
-jest.mock('axios')
-
 describe('<App />', () => {
+  let mockAxios: MockAdapter
+
+  beforeEach(() => {
+    mockAxios = new MockAdapter(axios)
+  })
+
+  afterEach(() => {
+    mockAxios.reset()
+  })
+
   it('should render search form', () => {
     render(<App />)
 
@@ -19,13 +28,14 @@ describe('<App />', () => {
 
     const expectedURL = 'https://viacep.com.br/ws/32110290/json/'
 
+    mockAxios.onGet('https://viacep.com.br/ws/32110290/json/').reply(200)
+
     const input = screen.getByRole('textbox')
     const button = screen.getByRole('button', { name: /buscar cep/i })
 
     userEvent.type(input, '32110290')
     userEvent.click(button)
 
-    expect(axios.get).toHaveBeenCalled()
-    expect(axios.get).toHaveBeenCalledWith(expectedURL)
+    expect(mockAxios.history.get[0].url).toEqual(expectedURL)
   })
 })
