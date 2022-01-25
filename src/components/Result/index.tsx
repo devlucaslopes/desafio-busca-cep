9f1c31e7567cb4f14fd65fb03627fe8bdd98edf1
Lucas Lopes <dev.thuram@gmail.com>
2022-01-25 08:12:11 -0300

    feat: result component receive props

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
 
-export const Result = () => (
+type ResultType = {
+  zipcode: string
+  state: string
+  city: string
+  address: string
+}
+
+export const Result = ({ zipcode, state, city, address }: ResultType) => (
   <ul>
     <li>
       <strong>CEP:</strong>
+      <span>{zipcode}</span>
     </li>
     <li>
       <strong>Estado:</strong>
+      <span>{state}</span>
     </li>
     <li>
       <strong>Cidade:</strong>
+      <span>{city}</span>
     </li>
     <li>
       <strong>Logradouro:</strong>
+      <span>{address}</span>
     </li>
   </ul>
 )
